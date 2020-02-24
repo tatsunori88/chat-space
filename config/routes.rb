88e77ca7feafab0c9d29670683560f77030b0d10
tatsunori88 <tatsunori.k.04.20.88@gmail.com>
2020-02-24 14:41:14 +0900

    chatspaceユーザー管理終了
    chatspaceユーザー管理（カリキュラム１）終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  #root "messages#index"
 
   root "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:edit, :update, :new, :create]
 end
